1e38a416769f0bb7748e2034f89779acd8768bad
Sofia555ya <sofiavashchuk75@gmail.com>
2025-05-03 15:52:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function button() {
+
+	let elem= document.getElementById("form");
+	elem.classList.toggle("work");
+}
+function button1() {
+
+	let elem1= document.getElementById("form1");
+	elem1.classList.toggle("work1");
+}
+function q1(){
+	let elem2= document.getElementById("an1");
+	elem2.classList.toggle("work2");
+}
+function q2(){
+	let elem3= document.getElementById("an2");
+	elem3.classList.toggle("work2");
+}
+function q3(){
+	let elem4= document.getElementById("an3");
+	elem4.classList.toggle("work2");
+}
+function q4(){
+	let elem5= document.getElementById("an4");
+	elem5.classList.toggle("work2");
+}
+function q5(){
+	let elem6= document.getElementById("an5");
+	elem6.classList.toggle("work2");
+}
+function text(){
+	let elem7= document.getElementById("form2");
+	elem7.classList.toggle("work3");
+}
+function text1(){
+	let elem8= document.getElementById("form3");
+	elem8.classList.toggle("work3");
+}
+function text2(){
+	let elem9= document.getElementById("form4");
+	elem9.classList.toggle("work3");
+}
